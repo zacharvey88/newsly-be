d9b11cd49ba1cc50f3336bce32468467b7fb2652
Zac <87400227+zacharvey88@users.noreply.github.com>
2024-04-17 11:13:39 +0100

    Added endpoint /api/users

--- models/users.js
+++ models/users.js
@@ -1,4 +1,10 @@
 const db = require('../db/connection')
 
+function findUsers() {
+  return db.query(`SELECT username, name, avatar_url FROM users`)
+  .then(({rows})=>{
+    return rows
+  })
+}
 
-module.exports = {}
+module.exports = {findUsers}
